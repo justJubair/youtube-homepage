88ca94cb957f876445b39b6aa0f5b169602a18fa
Jubair <jubair.ahmed2838@gmail.com>
2023-08-31 22:21:57 +0600

    data showed in UI from api

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 // Define UI variables
 const categoryContainer = document.getElementById('category-container');
+const videoContainer = document.getElementById('videos-container');
+
 const loadCategories = async ()=>{
     const res = await fetch('https://openapi.programming-hero.com/api/videos/categories');
     const data = await res.json();
@@ -10,7 +12,7 @@ const loadCategories = async ()=>{
         const categoryId = category.category_id;
         const div = document.createElement('div');
         div.innerHTML = `
-        <button class="btn">${category.category}</button>
+        <button onclick="handleLoadVideos(${categoryId})" class="btn">${category.category}</button>
         `;
         categoryContainer.appendChild(div);
 
@@ -19,3 +21,45 @@ const loadCategories = async ()=>{
 
 // load Categoriee function
 loadCategories();
+
+// handle load videos function
+const handleLoadVideos =(categoryId)=>{
+    // fetch data from the api, videos
+    const loadVideos = async()=>{
+    const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
+    const data = await res.json();
+    const trimedData = data.data;
+    
+    
+    // clear the video container element
+    videoContainer.innerHTML = '';
+    // loop through all the videos in the array
+        trimedData.forEach((video)=>{
+        console.log(video);
+        console.log(video.authors[0].verified);
+        const div = document.createElement('div');
+        div.className = 'card card-compact bg-base-100';
+        div.innerHTML = `
+        <figure><img src=${video?.thumbnail} alt="" /></figure>
+        <div class="mt-4">
+            <div class="flex items-start gap-2">
+                <div>
+                    <img class="w-9 rounded-full" src=${video.authors[0].profile_picture} />
+                </div>
+                <div>
+                    <h3 class="text-lg font-semibold">${video.title}</h3>
+                    <div class="flex items-center gap-2">
+                        <p>${video.authors[0].profile_name}</p>
+                        <span>${video.authors[0].verified ? '<i class="far fa-check-circle text-blue-600"></i>' : ''}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `;
+        videoContainer.appendChild(div);
+        });
+    };
+    // call the load videos function
+    loadVideos();
+}
+
