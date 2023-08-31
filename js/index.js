431aaeb09a33db7e383c937f28bbdf32c55d959b
Jubair <jubair.ahmed2838@gmail.com>
2023-08-31 20:23:01 +0600

    navbar added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
-console.log('hello');
+// Define UI variables
+const categoryContainer = document.getElementById('category-container');
+const loadCategories = async ()=>{
+    const res = await fetch('https://openapi.programming-hero.com/api/videos/categories');
+    const data = await res.json();
+    const trimedData = data.data;
+
+    // loop through all the categories and show them in the UI
+    trimedData.forEach((category)=>{
+        const categoryId = category.category_id;
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <button class="btn">${category.category}</button>
+        `;
+        categoryContainer.appendChild(div);
+
+    })
+}
+
+// load Categoriee function
+loadCategories();
