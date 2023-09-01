cb09f21e7fb1e1921098b536e9a4dd088e688845
Jubair <jubair.ahmed2838@gmail.com>
2023-09-01 14:38:37 +0600

    sort button functionality added

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ const categoryContainer = document.getElementById('category-container');
 const videoContainer = document.getElementById('videos-container');
 const noDataAvailable = document.getElementById('no-data-available-div');
 
-
+// load categories
 const loadCategories = async ()=>{
     const res = await fetch('https://openapi.programming-hero.com/api/videos/categories');
     const data = await res.json();
@@ -32,8 +32,7 @@ const handleLoadVideos =(categoryId)=>{
     // console.log(categoryId);
     const data = await res.json();
     const trimedData = data.data;
-    console.log(typeof trimedData);
-    console.log(trimedData);
+   
     // clear the video container element
     videoContainer.innerHTML = '';
     
@@ -90,8 +89,7 @@ const loadAllVideos = async()=>{
     // console.log(categoryId);
     const data = await res.json();
     const trimedData = data.data;
-    console.log(typeof trimedData);
-    console.log(trimedData);
+    
     // clear the video container element
     videoContainer.innerHTML = '';
     
@@ -145,4 +143,68 @@ loadAllVideos();
 // blog btn function
 const handleBlogBtn = ()=>{
     window.location.href = 'blog.html'
+};
+
+// sort btn function
+const handleSortBtn = async()=>{
+    const res = await fetch('https://openapi.programming-hero.com/api/videos/category/1000');
+    let data = await res.json();
+    data = data.data;
+    // console.log(data[0].others.views);
+    const customSort = (a,b)=>{
+        const viewA = parseInt(a.others.views);
+        const viewB = parseInt(b.others.views);
+        if(viewA < viewB) return 1;
+        else if(viewA > viewB) return -1;
+        return 0;
+    };
+    const trimedData = data.sort(customSort);
+
+    // clear the video container element
+    videoContainer.innerHTML = '';
+    
+    if(trimedData.length === 0) {
+       noDataAvailable.innerHTML = `
+       <div><img src="./images/Icon.png"></div>
+       <h2 class="text-2xl font-bold text-center">Oops!! Sorry, There is no<br> content here</h2>
+       `;
+       noDataAvailable.classList.remove('hidden');
+       return noDataAvailable;
+    } else {
+        noDataAvailable.classList.add('hidden')
+    }
+
+    // loop through all the videos in the array
+    trimedData.forEach((video)=>{
+        // console.log(video);
+        // console.log(typeof video.others.posted_date);
+        const seconds = parseInt(video?.others?.posted_date);
+        // console.log(seconds);
+        const div = document.createElement('div');
+        div.className = 'card-compact bg-base-100';
+        div.innerHTML = `
+        <figure><img class="h-[200px] w-full md:w-[300px] rounded-lg" src=${video?.thumbnail} alt="" /></figure>
+        <div class="absolute bg-black opacity-70 text-white text-xs p-1 -mt-8 ml-52 md:-mt-8 md:ml-40">
+           <p>${isNaN(seconds) ? '' : (JSON.stringify(Math.floor(seconds/3600)).concat('hrs'))} <span>${isNaN(seconds) ? '' : (JSON.stringify(Math.floor((seconds/60)%60)).concat(' ', 'min ago'))}</span</p>
+        </div>
+        <div class="mt-4">
+            <div class="flex items-start gap-2">
+                <div>
+                    <img class="w-9 rounded-full" src=${video?.authors[0]?.profile_picture} />
+                </div>
+                <div>
+                    <h3 class="text-lg font-bold">${video?.title}</h3>
+                    <div class="flex items-center gap-2">
+                        <p class="text-gray-500">${video?.authors[0]?.profile_name}</p>
+                        <span>${video?.authors[0]?.verified ? '<i class="far fa-check-circle text-blue-600"></i>' : ''}</span>
+                    </div>
+                    <p class="text-gray-500">${video?.others?.views}</p>
+                </div>
+            </div>
+        </div>
+        `;
+
+        videoContainer.appendChild(div);
+        });
+    
 };
